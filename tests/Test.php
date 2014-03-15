64bf44490fc40d34c085289ecef6e81f4b2d845d
Jesse Schalken <me@jesseschalken.com>
2014-03-15 18:43:20 +1100

    Move testing to PhpUnit

--- tests/Test.php
+++ tests/Test.php
@@ -6,81 +6,30 @@ use DirectoryIterator;
 use RegexIterator;
 use SplFileInfo;
 
-class MustacheTest extends Test {
-    protected function name() {
-        return 'Official Mustache spec';
-    }
-
-    protected function description() {
-        return "These are the official mustache specs\n";
-    }
-
-    protected function subTests() {
-        $tests = array();
-
+class MustacheTest extends \PHPUnit_Framework_TestCase {
+    function testMustacheSpec() {
         /** @var SplFileInfo $file */
-        foreach ($this->specFiles() as $file) {
-            $test               = new MustacheSpecFile;
-            $test->specFileName = $file->getFilename();
-            $test->specFileJson = json_decode(file_get_contents($file->getPathname()));
-            $tests[]            = $test;
+        $files = new RegexIterator(new DirectoryIterator(__DIR__ . "/spec/specs"), '/^[^~].*\.json$/');
+        foreach ($files as $file) {
+            $name = $file->getFilename();
+            echo "Testing $name...\n";
+
+            $json1 = json_decode(file_get_contents($file->getPathname()), true);
+
+            foreach ($json1['tests'] as $json) {
+                $name     = $json['name'];
+                $data     = $json['data'];
+                $template = $json['template'];
+                $expected = $json['expected'];
+                $partials = isset($json['partials']) ? $json['partials'] : array();
+
+                echo "  Testing $name...\n";
+
+                $result = Mustache::run($template, $data, $partials);
+
+                $this->assertEquals($expected, $result);
+            }
         }
-
-        return $tests;
-    }
-
-    private function specFiles() {
-        return new RegexIterator(new DirectoryIterator(__DIR__ . "/spec/specs"), '/^[^~].*\.json$/');
-    }
-}
-
-class MustacheSpecFile extends Test {
-    var $specFileName;
-    var $specFileJson;
-
-    protected function name() {
-        return $this->specFileName;
-    }
-
-    protected function description() {
-        return $this->specFileJson->overview;
-    }
-
-    protected function subTests() {
-        $tests = array();
-
-        foreach ($this->specFileJson->tests as $jsonTest) {
-            $test           = new MustacheTestCase;
-            $test->jsonTest = $jsonTest;
-            $tests[]        = $test;
-        }
-
-        return $tests;
-    }
-}
-
-class MustacheTestCase extends Test {
-    var $jsonTest;
-
-    protected function name() {
-        return $this->jsonTest->name;
-    }
-
-    protected function description() {
-        return $this->jsonTest->desc . "\n";
-    }
-
-    protected function runTest() {
-        $data     = $this->jsonTest->data;
-        $template = $this->jsonTest->template;
-        $expected = $this->jsonTest->expected;
-
-        $partials = @$this->jsonTest->partials;
-        $partials = isset($partials) ? (array)$partials : array();
-
-        $result = Mustache::run($template, $data, $partials);
-
-        $this->assertEquals($expected, $result);
     }
 }
 
