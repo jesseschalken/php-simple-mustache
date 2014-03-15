e02d6a51e46cbb6188d8847252c67323f0ef36a4
Jesse Schalken <me@jesseschalken.com>
2014-03-15 16:44:30 +1100

    Use composer for autloading

--- Test.php
+++ Test.php
@@ -24,7 +24,7 @@ class MustacheTest extends Test {
     }
 
     private function specFiles() {
-        return new RegexIterator(new DirectoryIterator(dirname(__FILE__) . "/spec/specs"), '/^[^~].*\.json$/');
+        return new RegexIterator(new DirectoryIterator(__DIR__ . "/spec/specs"), '/^[^~].*\.json$/');
     }
 }
 
